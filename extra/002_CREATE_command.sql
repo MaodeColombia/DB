6a1975bd2886c8c6668a4e66ca07a60a6d644b1c
Mauricio Martinez Orjuela <maodecolombia@gmail.com>
2022-08-21 22:06:44 -0500

    borrar la Linea de Comandos

---
+++ extra/002_CREATE_command.sql
@@ -0,0 +1,3 @@
+-- como se se ha estado usando la CL - Linea de Comandos, un comando que es util se este que sirve para borrar la pantalla
+system cls; 
+-- https://github.com/MaodeColombia/DB/blob/main/extra/image/20220821220437.png
