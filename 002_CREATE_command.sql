57f91218cbb9a4096f04559a0e2376c9016c243a
Mauricio Martinez Orjuela <maodecolombia@gmail.com>
2022-08-21 23:33:44 -0500

    puntero sobre la nueva DB creada

--- 002_CREATE_command.sql
+++ 002_CREATE_command.sql
@@ -15,7 +15,13 @@ SHOW DATABASES;
 -- https://github.com/MaodeColombia/DB/blob/main/extra/image/20220821223041.png
 
 
+
 -- NOTE: si se usa el comando "IF NOT EXISTS" cuando se crea una DB, evitamos que se vuelva un error
 CREATE DATABASE plataform_operation;
 CREATE DATABASE IF NOT EXISTS plataform_operation;
 -- https://github.com/MaodeColombia/DB/blob/main/extra/image/20220821224141.png
+
+
+SELECT DATABASE(); -- para conocer a dónde está el puntero
+USE PLATAFORM_OPERATION; -- se mueve el puntero a la DB que se acaba de crear
+--NOTE: notese que que el nombre de la DB cuando se creó "plataform_operation" fue con letras minusculas y aquí cuando se uso el nombre de la DB se uso con mayuscula sostenida, con esto se quiere dar a entender que MySQL "No es sensible a mayúsculas/minúsculas"
