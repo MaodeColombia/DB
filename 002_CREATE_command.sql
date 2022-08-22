4b904bd084759d1ca361d6c00083ee4df74794f0
Mauricio Martinez Orjuela <maodecolombia@gmail.com>
2022-08-21 22:21:48 -0500

    Lectura recomendada

--- 002_CREATE_command.sql
+++ 002_CREATE_command.sql
@@ -1,3 +1,9 @@
 -- como se se ha estado usando la CL - Linea de Comandos, un comando que es util se este que sirve para borrar la pantalla
 system cls; 
 -- https://github.com/MaodeColombia/DB/blob/main/extra/image/20220821221621.png
+
+/*
+LECTURA RECOMENDADA:
+InnoDB y MyISAM: El ying yang de MySQL ☯🐬
+https://platzi.com/tutoriales/1272-sql-mysql/4484-innodb-y-myisam-el-ying-yang-de-mysql/
+*/
