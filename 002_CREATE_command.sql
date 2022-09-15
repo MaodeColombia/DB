a166692b6ea51144754eb4957ea021b32e50ad0f
Mauricio Martinez Orjuela <maodecolombia@gmail.com>
2022-09-15 11:31:51 -0500

    version final del comando CREATE TABLE

--- 002_CREATE_command.sql
+++ 002_CREATE_command.sql
@@ -28,14 +28,14 @@ USE PLATAFORM_OPERATION; -- se mueve el puntero a la DB que se acaba de crear
 -- https://github.com/MaodeColombia/DB/blob/main/extra/image/20220821225851.png
 
 
-CREATE TABLE books IF NOT EXISTS(
+CREATE TABLE IF NOT EXISTS books(
     `book_id` INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     `author` INTEGER UNSIGNED,
-    title VARCHAR(100) NOT NULL,
+    `title` VARCHAR(100) NOT NULL,
     `year` INTEGER UNSIGNED NOT NULL DEFAULT 1900,
     `language` VARCHAR(2) NOT NULL DEFAULT 'es' COMMENT 'ISO 639-1 Language',
     `cover_url` VARCHAR(500),
-    price DOUBLE(6,2) NOT NULL DEFAULT 10.0,
+    `price` DOUBLE(6,2) NOT NULL DEFAULT 10.0,
     `sellable` TINYINT(1) DEFAULT 1,
     `description` TEXT
 );
